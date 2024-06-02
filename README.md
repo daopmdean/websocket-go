640e24a3785513e6a6cbeb1cc32ed8c5c14f423e
Dao Pham <daopm.dean@gmail.com>
2024-06-02 23:24:03 +0700

    implementation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # websocket-go
+
+Basic demo for golang websocket<br>
+Using: golang gorilla websocket for backend, vuejs for frontend
+
+## Get started
+
+```
+go run main.go
+```
+
+go to -> localhost:8080<br>
+to test the simple chat app<br>
+open new broswer for try out difference users<br>
--- main.go
+++ main.go
@@ -1,1 +1,66 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+var upgrader = websocket.Upgrader{}
+
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	log.Print("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg Message
+		if err := ws.ReadJSON(&msg); err != nil {
+			log.Printf("err: %v\n", err)
+			delete(clients, ws)
+			break
+		}
+
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+
+		for client := range clients {
+			if err := client.WriteJSON(msg); err != nil {
+				log.Printf("err: %v\n", err)
+				delete(clients, client)
+				client.Close()
+			}
+		}
+	}
+}
